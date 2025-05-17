b21aded9f9db343e015e9d9b4b6addb10f82e46c
Muhammad Waleed Abdullah <w26waleed@gmail.com>
2025-05-17 18:31:37 +0500

    feat: add fade up animation

---
+++ src/modules/navbar-layout/layout/fade-up-animation/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { motion } from "motion/react";
+
+import { type PropsWithChildren } from "@/core/types/common";
+
+export const FadeUpAnimation = ({ children }: PropsWithChildren) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className="size-full flex flex-1"
+    >
+      {children}
+    </motion.div>
+  );
+};
